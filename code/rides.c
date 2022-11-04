05b7ef38ddaa88ce9c4bd758be55ae127b726561
a96807 <a96807@alunos.uminho.pt>
2022-11-04 17:34:50 +0000

    inicio

---
+++ code/rides.c
@@ -0,0 +1,14 @@
+#include rides.h;
+
+struct ride {
+	char *id;
+	char *date;
+	char *driver;
+	char *user;
+	char *city;
+	char *distance;
+	char *score_user;
+	char *score_driver;
+	char *tip;
+	char *comment;
+}
---
+++ code/rides.h
@@ -0,0 +1,8 @@
+#ifndef RIDES_H;
+#define RIDES_H;
+
+#define MaxRides 1000000;
+typedef struct ride *RIDES;
+
+
+#endif
