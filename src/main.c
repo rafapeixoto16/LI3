2cc7797dc2e08cc696c6bcaa13d27eadd696d053
a96807 <a96807@alunos.uminho.pt>
2022-11-04 22:07:16 +0000

    mudança nome pasta

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main (){
-    FILE *foDrivers = fopen("Dataset_Fase1/","r");
-
-    printf("merda");
+    printf("aqui");
+    FILE *fpDrivers;
+    fpDrivers = fopen ("../Dataset_Fase1/drivers.cvs","r");
+    char linha[500];
+    int n =fgets(linha,500,fpDrivers);
+    if (n!=NULL)
+        printf("d");
     return 0;
 }
