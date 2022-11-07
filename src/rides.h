d390c5a7b8c6ee0a5989c177aa3ee9771e266fab
diogosilves <a97941@alunos.uminho.pt>
2022-11-07 19:51:50 +0000

    rides organizado

--- src/rides.h
+++ src/rides.h
@@ -9,19 +9,69 @@
 #define hashMaxRides 1000000
 
 #define dataDia "9/10/2022"
-
+/*!
+* Define a struct ride.
+*/
 typedef struct ride *RIDES;
 
+/*!
+* Esta função transforma o id de string para um integer.
+* @param id O id.
+* @return A localização na tabela.
+*/
 unsigned int hashRides(char *id);
+
+/*!
+* Função que inicia a hashtable com posições a nulo.
+*/
 void initHashTableRides();
+
+/*!
+* Função que dá print da hash table.
+*/
 void printTableRides();
-void ridesParsing(char *linha);
-RIDES hashTableLookupRides(char *id);
-char *lookupRides(char *i);
-char *lookupDriverRides(int i);
-int lookupDistanceRides(int i);
-bool lookupCidadeRides(int i ,char *cidade);
+
+/*!
+ *
+ * @param ride
+ * @return
+ */
 bool hashTableInsertRides(RIDES ride);
+
+/*!
+* Procura por um dado condutor (driver).
+* @param id Id do condutor esperado.
+* @return Dados do condutor (driver).
+*/
+RIDES hashTableLookupRides(char *id);
+
+/*!
+* Função que
+* @param linha Dados do condutor (driver).
+*/
+void ridesParsing(char *linha);
+
+/*!
+*
+* @param i
+* @return
+*/
+int lookupDistanceRides(int i);
+
+/*!
+*
+* @param i
+* @return
+*/
+char *lookupDriverRides(int i);
+
+/*!
+*
+* @param i
+* @param cidade
+* @return
+*/
+bool lookupCidadeRides(int i ,char *cidade);
 
 
 #endif
