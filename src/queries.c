144208393605289ef0b22788e940f03526236e88
diogosilves <a97941@alunos.uminho.pt>
2022-11-10 22:32:49 +0000

    switch case added in queries.c

--- src/queries.c
+++ src/queries.c
@@ -4,6 +4,7 @@
 
 #include "queries.h"
 
+/*
 char parsingQueries(char *linhaQ){
     char *query;//primeira parte da linha
     if(strcmp("4",query)==0){
@@ -17,4 +18,52 @@ char parsingQueries(char *linhaQ){
 
     }
     else return NULL;
+}
+ */
+
+char parsingQueries(char *linhaQ){
+    char *query;
+
+    switch(*query){
+
+        case 1:{
+            //query1
+        }
+
+        case 2:{
+            //query2
+        }
+
+        case 3:{
+            //query3
+        }
+
+        case 4:{
+            query4(NULL);
+        }
+
+        case 5:{
+            //query5
+        }
+
+        case 6:{
+            //query6
+        }
+
+        case 7:{
+            //query7
+        }
+
+        case 8:{
+            //query8
+        }
+
+        case 9:{
+            //query9
+        }
+
+        default:
+            return NULL;
+    }
+
 }
